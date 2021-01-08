f19c04d9bf9be93483df90c06ab41baeb84d106c
Amarachi <ajoenwek@umd.edu>
2021-01-08 10:49:49 -0700

    updated comparison to use contains method

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -2,11 +2,11 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 
-public class PacMan{
+public class PacMan {
 	String myName;
 	Location myLoc;
 	Map myMap;
-	Location shift; 
+	Location shift;
 
 	public PacMan(String name, Location loc, Map map) {
 		this.myLoc = loc;
@@ -15,25 +15,29 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		
+
 		ArrayList<Location> moves = new ArrayList<>();
-		
-		//checks for the position to the right the pacman
-		if( myMap.getLoc(myLoc.shift(1,0)) == Map.Type.COOKIE || myMap.getLoc(myLoc.shift(1,0)) == Map.Type.EMPTY)
-			moves.add(myLoc.shift(1,0));
-	
-		//checks for the position above the pacman
-		if( myMap.getLoc(myLoc.shift(0,1)) == Map.Type.COOKIE || myMap.getLoc(myLoc.shift(0,1)) == Map.Type.EMPTY)
-			moves.add(myLoc.shift(0,1));
-	
-		//checks for the position to the left the pacman
-		if( myMap.getLoc(myLoc.unshift(1,0)) == Map.Type.COOKIE || myMap.getLoc(myLoc.unshift(1,0)) == Map.Type.EMPTY)
-			moves.add(myLoc.unshift(1,0));
-		
-		//checks for the position below the pacman
-		if( myMap.getLoc(myLoc.unshift(0,1)) == Map.Type.COOKIE || myMap.getLoc(myLoc.unshift(0,1)) == Map.Type.EMPTY)
-			moves.add(myLoc.unshift(0,1));
-	
+
+		// checks for the position to the right the pacman
+		if (myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.COOKIE)
+				|| myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.EMPTY))
+			moves.add(myLoc.shift(1, 0));
+
+		// checks for the position above the pacman
+		if (myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.COOKIE)
+				|| myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.EMPTY))
+			moves.add(myLoc.shift(0, 1));
+
+		// checks for the position to the left the pacman
+		if (myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.COOKIE)
+				|| myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.EMPTY))
+			moves.add(myLoc.shift(-1, 0));
+
+		// checks for the position below the pacman
+		if (myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.COOKIE)
+				|| myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.EMPTY))
+			moves.add(myLoc.shift(0, -1));
+
 		return moves;
 	}
 
@@ -41,11 +45,11 @@ public class PacMan{
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
+	public boolean is_ghost_in_range() {
 		return false;
 	}
 
-	public JComponent consume() { 
- 		return null;
+	public JComponent consume() {
+		return null;
 	}
 }
