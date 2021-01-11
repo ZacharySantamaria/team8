41ef5397bf82299cdbd72b88c451a4d472466deb
Amarachi <ajoenwek@umd.edu>
2021-01-11 16:41:51 -0700

    updated get valid moves to use assertion

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,13 +1,17 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException {
 		NoFrame frame = new NoFrame();
-		PacMan pacman = frame.addPacMan(new Location(2, 2));
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
 
-		System.out.println(pacman.get_valid_moves());
+		ArrayList<Location> ans = new ArrayList<>();
+		ans.add(new Location(2, 1));
+		ans.add(new Location(1, 2));
+		assertEquals(ans, pacman.get_valid_moves());
 	}
 }
