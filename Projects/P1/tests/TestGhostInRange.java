04409169eb899348fcc7990be3889c543b081fd5
jdrozar3 <rozariomichael473@gmail.com>
2021-01-11 18:30:49 -0500

    update test

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -1,11 +1,14 @@
+import java.io.FileNotFoundException;
+
 import junit.framework.*;
-import java.awt.Color;
-import java.io.*;
 
 public class TestGhostInRange extends TestCase {
 
-	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+	public void testGhostInRange() throws FileNotFoundException {
+		NoFrame frame = new NoFrame();
+		Location loc = new Location(1, 1);
+		PacMan pacman = frame.addPacMan(loc);
+		Ghost ghost = frame.addGhost(new Location(1, 2), "ghost", null);
+		System.out.println(pacman.is_ghost_in_range());
 	}
 }
-
