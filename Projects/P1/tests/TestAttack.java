eb020c1f755e00d56d3fa4f15f06b6f17b0ccd68
Amarachi <ajoenwek@umd.edu>
2021-01-11 14:00:02 -0700

    implemented test attack

--- Projects/P1/tests/TestAttack.java
+++ Projects/P1/tests/TestAttack.java
@@ -4,7 +4,10 @@ import java.io.*;
 
 public class TestAttack extends TestCase {
 
-	public void testAttack() throws FileNotFoundException{
-		return null;	
+	public void testAttack() throws FileNotFoundException {
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(12, 1), "name", Color.red);
+		PacMan pacman = frame.addPacMan(new Location(13, 1));
+		System.out.println(ghost.attack());
 	}
 }
