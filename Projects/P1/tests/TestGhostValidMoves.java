ffa1112127de58afb80b293c6bb3d149d0c6c052
rogerluo-zyz <rogerluo0526@foxmail.com>
2021-01-12 09:05:08 +0800

    TestGhostValidMoves

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Location loc = new Location(1, 1);
+		Ghost g = frame.addGhost(loc, "ghost", Color.red); 
+		ArrayList<Location> result = g.get_valid_moves();
+		ArrayList<Location> valid_moves = new ArrayList<>();
+		valid_moves.add(new Location(2,1));
+		valid_moves.add(new Location(1,2));		
+		assertEquals(result, valid_moves);
 	}
 }
