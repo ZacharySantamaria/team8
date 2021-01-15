aa10bd907391aafb3918061aa85eb667b76fe4b9
rogerluo-zyz <61128507+rogerluo-zyz@users.noreply.github.com>
2021-01-16 04:27:56 +0800

    Update PacMan.java

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -44,10 +44,20 @@ public class PacMan{
 
 	public boolean move() {
 		ArrayList<Location> locations = this.get_valid_moves();
-
+		if(this.is_ghost_in_range){
+			for(Location l: locations){
+				if(map.getLoc(l).contains(Map.Type.GHOST)){
+					locations.remove(l);
+				}
+			}
+		}
+		if(locations.size() > 1 && prevLoc != null){
+			locations.remove(l);
+		}		
 		if (locations.size() == 0)
 			return false;
-    
+		
+		this.prevLoc = this.myLoc;
 		this.myLoc = locations.get(0);
 		this.myMap.move(myName, myLoc, Map.Type.PACMAN);
 		
@@ -56,6 +66,36 @@ public class PacMan{
 	}
 
 	public boolean is_ghost_in_range() { 
+		Location up = this.myLoc.shift(0, 1);
+		this.myLoc = this.myLoc.unshift(up);
+
+		Location down = this.myLoc.shift(0, -1);
+		this.myLoc = this.myLoc.unshift(down);
+
+		Location left = this.myLoc.shift(-1, 0);
+		this.myLoc = this.myLoc.unshift(left);
+
+		Location right = this.myLoc.shift(1, 0);
+		this.myLoc = this.myLoc.unshift(right);
+
+		Location upright = this.myLoc.shift(1, 1);
+		this.myLoc = this.myLoc.unshift(upright);
+
+		Location downright = this.myLoc.shift(1, -1);
+		this.myLoc = this.myLoc.unshift(downright);
+
+		Location upleft = this.myLoc.shift(-1, 1);
+		this.myLoc = this.myLoc.unshift(upleft);
+
+		Location downleft = this.myLoc.shift(-1, -1);
+		this.myLoc = this.myLoc.unshift(downleft);
+
+		if (myMap.getLoc(up).contains(Map.Type.GHOST) || myMap.getLoc(down).contains(Map.Type.GHOST)
+				|| myMap.getLoc(left).contains(Map.Type.GHOST) || myMap.getLoc(right).contains(Map.Type.GHOST)
+				|| myMap.getLoc(upright).contains(Map.Type.GHOST) || myMap.getLoc(downright).contains(Map.Type.GHOST)
+				|| myMap.getLoc(upleft).contains(Map.Type.GHOST) || myMap.getLoc(downleft).contains(Map.Type.GHOST)) {
+			return true;
+		}
 		return false;
 	}
 
