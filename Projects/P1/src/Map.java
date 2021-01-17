ba17d5704e6ac67a617e835116b77aa4148f6c69
Aymen Mehai <aymehaidev@protonmail.com>
2021-01-17 14:43:28 -0500

    Fixed Sabotage

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -66,7 +66,7 @@ public class Map {
 			return new HashSet<Type>();
 		}
 
-		if (field.get(loc).contains(Map.Type.WALL)) {
+		if (field.get(loc).contains(Map.Type.WALL))
 			return wallSet;
 		}
 
@@ -74,7 +74,7 @@ public class Map {
 			return emptySet;
 
 		else
-			return field.get(new Location(0,0));
+			return field.get(loc);
 	}
 
 	// this is only called by ghost attack
