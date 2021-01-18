8a4cda1a827a499f862969fe86c6e33da471fd5b
yairnbar <yairnbar@gmail.com>
2021-01-17 19:55:53 -0500

    fixed sabatoged test...

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -6,17 +6,11 @@ public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
-		Location loc = new Location(3, 21);
+		Location loc = new Location(1, 11);
 		PacMan pacman = frame.addPacMan(loc); 
-		for(int i = 0; i < 19; i++) {
-			pacman.move();
-		}
-		assertEquals(pacman.myLoc, new Location(2,3));
-		for(int i = 0; i < 9; i++) {
-			pacman.move();
-		}
-		assertEquals(pacman.myLoc, new Location(1,11));
 		pacman.move();
 		assertEquals(pacman.myLoc, new Location(1,10));
+		pacman.move();
+		assertEquals(pacman.myLoc, new Location(1,9));
 	}
 }
